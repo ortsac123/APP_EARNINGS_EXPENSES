9eb27848a72b0cb944be25e1c9ee7f5ea7cdc36d
Rafael CC <rafakastro27@gmail.com>
2023-08-24 15:26:39 -0500

    Creacion del proyecto con vitr

---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+import React from 'react'
+
+export const GlobalState = () => {
+
+    const context = createContext()
+
+    
+  return (
+    <div>GlobalState</div>
+  )
+}
